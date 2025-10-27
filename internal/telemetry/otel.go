e104b77ac8cd0fe6721ea57add75519e290744a2
Máté Czékus <mate@picloud.hu>
2025-10-27 23:13:51 +0100

    feat: wire up open telemetry collectors
    Signed-off-by: Máté Czékus <mate@picloud.hu>

---
+++ internal/telemetry/otel.go
@@ -0,0 +1,47 @@
+// Package telemetry contains configuration funcs for application telemetry data like logging or tracing.
+package telemetry
+
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/contrib/exporters/autoexport"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+// SetupOTel creates a new open telemetry trace and metric provider and sets them on the global context.
+//
+// ctx is the current context that we use to set these metrics up.
+//
+// Returns a shutdown function and error if any.
+func SetupOTel(ctx context.Context) (func(context.Context) error, error) {
+	se, err := autoexport.NewSpanExporter(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("span exporter creation: %w", err)
+	}
+
+	tp := trace.NewTracerProvider(trace.WithBatcher(se))
+	otel.SetTracerProvider(tp)
+
+	mr, err := autoexport.NewMetricReader(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("metric reader creation: %w", err)
+	}
+
+	mp := metric.NewMeterProvider(metric.WithReader(mr))
+	otel.SetMeterProvider(mp)
+
+	return func(sCtx context.Context) error {
+		if sErr := tp.Shutdown(sCtx); sErr != nil {
+			return fmt.Errorf("trace provider shutdown: %w", sErr)
+		}
+
+		if sErr := mp.Shutdown(sCtx); sErr != nil {
+			return fmt.Errorf("metric provider shutdown: %w", sErr)
+		}
+
+		return nil
+	}, nil
+}
